e67553351ad1e07856a451c937df7c6449ef4de5
shafiul-osd <mdshafiulislamsafi2506@gmail.com>
2024-05-10 15:34:03 +0600

    Navbar conditionalized

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import {
     Navbar,
     MobileNav,
@@ -15,8 +15,20 @@ import {
 } from "@material-tailwind/react";
 import { Link, NavLink } from "react-router-dom";
 import { ChevronDownIcon, Cog6ToothIcon, InboxArrowDownIcon, LifebuoyIcon, PowerIcon, UserCircleIcon } from "@heroicons/react/16/solid";
+import { AuthContext } from "../../Providers/AuthProvider/AuthProvider";
+import { signOut } from "firebase/auth";
+import auth from "../../firebase/firebase.config";
 
 export default function StickyNavbar() {
+    const { user } = useContext(AuthContext);
+    const handleLogOut = () => {
+        signOut(auth)
+            .then((result) => {
+                alert("success")
+            }).catch((error) => {
+                alert(error.message)
+            });
+    }
     const [openNav, setOpenNav] = React.useState(false);
 
     React.useEffect(() => {
@@ -91,81 +103,93 @@ export default function StickyNavbar() {
                 <div className="flex items-center gap-3 md:gap-10">
                     <div className="mx-auto hidden lg:block">{navList}</div>
                     <div className="flex items-center gap-x-1 ">
-                        <Button
-                            variant="text"
-                            size="sm"
-                            className="hidden lg:inline-block"
-                        >
-                            <Link to="/login">Log In</Link>
-                        </Button>
-                        <Button
-                            variant="gradient"
-                            size="sm"
-                            className="hidden lg:inline-block"
-                        >
-                            <Link to="/register">Sign in</Link>
-                        </Button>
-                    </div>
-                    <Menu open={isMenuOpen} handler={setIsMenuOpen} placement="bottom-end">
+                        {
+                            user ? <Button onClick={handleLogOut} variant="danger" size="sm" className="hidden lg:inline-block"> <Link to="/login">Log Out</Link>
+                            </Button>
+                                :
+                                <>
+                                    <Button
+                                        variant="text"
+                                        size="sm"
+                                        className="hidden lg:inline-block"
+                                    >
+                                        <Link to="/login">Log In</Link>
+                                    </Button>
+                                    <Button
+                                        variant="gradient"
+                                        size="sm"
+                                        className="hidden lg:inline-block"
+                                    >
+                                        <Link to="/register">Sign in</Link>
+                                    </Button>
+                                </>
+                        }
                         <label className="swap swap-rotate m-0 p-0">
                             <input type="checkbox" className="theme-controller" value="synthwave" />
                             <svg className="swap-off fill-current w-8 h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" /></svg>
                             <svg className="swap-on fill-current w-8 h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" /></svg>
                         </label>
-                        <MenuHandler>
-                            <Button
-                                variant="text"
-                                color="blue-gray"
-                                className="flex items-center gap-1 rounded-full py-0.5 pr-2 pl-0.5 lg:ml-auto"
-                            >
-                                <Tooltip content="Material Tailwind" placement="bottom-end" animate={{
-                                    mount: { scale: 1, y: 0 },
-                                    unmount: { scale: 0, y: 0 },
-                                }}>
-                                    <Avatar
-                                        variant="circular"
-                                        size="sm"
-                                        alt="tania andrew"
-                                        className="border border-gray-900 p-0.5"
-                                        src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
-                                    />
-                                </Tooltip>
-                                <ChevronDownIcon
-                                    strokeWidth={2.5}
-                                    className={`h-3 w-3 transition-transform ${isMenuOpen ? "rotate-180" : ""
-                                        }`}
-                                />
-                            </Button>
-                        </MenuHandler>
-                        <MenuList className="p-1">
-                            {profileMenuItems.map(({ label, icon }, key) => {
-                                const isLastItem = key === profileMenuItems.length - 1;
-                                return (
-                                    <MenuItem
-                                        key={label}
-                                        onClick={closeMenu}
-                                        className={`flex items-center gap-2 rounded ${isLastItem
-                                            ? "hover:bg-red-500/10 focus:bg-red-500/10 active:bg-red-500/10"
-                                            : ""
-                                            }`}
+                    </div>
+                    {
+                        user &&
+                        <>
+                            <Menu open={isMenuOpen} handler={setIsMenuOpen} placement="bottom-end">
+                                <MenuHandler>
+                                    <Button
+                                        variant="text"
+                                        color="blue-gray"
+                                        className="flex items-center gap-1 rounded-full py-0.5 pr-2 pl-0.5 lg:ml-auto"
                                     >
-                                        {React.createElement(icon, {
-                                            className: `h-4 w-4 ${isLastItem ? "text-red-500" : ""}`,
-                                            strokeWidth: 2,
-                                        })}
-                                        <Typography
-                                            as="span"
-                                            variant="small"
-                                            className="font-normal"
-                                            color={isLastItem ? "red" : "inherit"}
-                                        >
-                                            {label}
-                                        </Typography>
-                                    </MenuItem>
-                                );
-                            })}
-                        </MenuList>
-                    </Menu>
+                                        <Tooltip content={`${user?.displayName}`} placement="bottom-end" animate={{
+                                            mount: { scale: 1, y: 0 },
+                                            unmount: { scale: 0, y: 0 },
+                                        }}>
+                                            <Avatar
+                                                variant="circular"
+                                                size="sm"
+                                                alt="tania andrew"
+                                                className="border border-gray-900 p-0.5"
+                                                src={`${user?.photoURL}`}
+                                            />
+                                        </Tooltip>
+                                        <ChevronDownIcon
+                                            strokeWidth={2.5}
+                                            className={`h-3 w-3 transition-transform ${isMenuOpen ? "rotate-180" : ""
+                                                }`}
+                                        />
+                                    </Button>
+                                </MenuHandler>
+                                <MenuList className="p-1">
+                                    {profileMenuItems.map(({ label, icon }, key) => {
+                                        const isLastItem = key === profileMenuItems.length - 1;
+                                        return (
+                                            <MenuItem
+                                                key={label}
+                                                onClick={closeMenu}
+                                                className={`flex items-center gap-2 rounded ${isLastItem
+                                                    ? "hover:bg-red-500/10 focus:bg-red-500/10 active:bg-red-500/10"
+                                                    : ""
+                                                    }`}
+                                            >
+                                                {React.createElement(icon, {
+                                                    className: `h-4 w-4 ${isLastItem ? "text-red-500" : ""}`,
+                                                    strokeWidth: 2,
+                                                })}
+                                                <Typography
+                                                    as="span"
+                                                    variant="small"
+                                                    className="font-normal"
+                                                    color={isLastItem ? "red" : "inherit"}
+                                                >
+                                                    {label}
+                                                </Typography>
+                                            </MenuItem>
+                                        );
+                                    })}
+                                </MenuList>
+                            </Menu>
+                        </>
+                    }
                     <IconButton
                         variant="text"
                         className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
@@ -208,12 +232,18 @@ export default function StickyNavbar() {
             <MobileNav open={openNav}>
                 {navList}
                 <div className="flex items-center gap-x-1">
-                    <Button fullWidth variant="text" size="sm" className="">
-                        <Link to="/login">Log In</Link>
-                    </Button>
-                    <Button fullWidth variant="gradient" size="sm" className="">
-                        <Link to="/register" >Sign in</Link>
-                    </Button>
+                    {
+                        user ? <Button onClick={handleLogOut} fullWidth variant="danger" size="sm"> <Link to="/login">Log Out</Link></Button>
+                            :
+                            <>
+                                <Button fullWidth variant="text" size="sm" className="">
+                                    <Link to="/login">Log In</Link>
+                                </Button>
+                                <Button fullWidth variant="gradient" size="sm" className="">
+                                    <Link to="/register" >Sign in</Link>
+                                </Button>
+                            </>
+                    }
                 </div>
             </MobileNav>
         </Navbar>
